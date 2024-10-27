eb92b86b831132fbd6968a38cf796528e44ff5c5
Sol Wolf <wolfendenbuild@gmail.com>
2024-10-27 15:35:52 +1000

    Test out grid system and scrolling

--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -1,8 +1,10 @@
 extends Camera2D
 
-const speed: float = 500
+const speed: float = 800
 
 func _process(delta: float) -> void:
-	var axis = Input.get_axis("ui_up", "ui_down")
+	var axis = Input.get_axis("up", "down")
 	var new_y = position.y + axis * delta * speed
-	position.y = clamp(new_y, 0, $"..".total_height * $"..".tile_height + get_viewport_rect().size.y)
+	var min = 0
+	var max = $"..".total_height * $"..".tile_height - get_viewport_rect().size.y
+	position.y = clamp(new_y, min, max)
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -8,35 +8,56 @@ const base_height: int = 7
 const no_mans_land_height = 9
 const total_height: int = 2 * base_height + no_mans_land_height
 
-var tiles: Array[int]
+var buildings_p1: Array[Building]
+var units_p1: Array[Unit]
+var buildings_p2: Array[Building]
+var units_p2: Array[Unit]
 
 func _init() -> void:
-	tiles = []
+	buildings_p1 = []
+	units_p1 = []
+	buildings_p2 = []
+	units_p2 = []
+
+func _ready() -> void:
+	$Board.position.x = (get_viewport_rect().size.x - (board_width * tile_width)) / 2
+
+func _process(delta: float) -> void:
+	$Board.position.x = (get_viewport_rect().size.x - (board_width * tile_width)) / 2
 
 func _draw() -> void:
-	var horizontal_start = 0
-	var horizontal_end = get_viewport_rect().size.x
+	var horizontal_start = $Board.position.x
+	var horizontal_end = board_width * tile_width + $Board.position.x
 	
 	var vertical_start = 0
 	var vertical_end = total_height * tile_height
 	
 	# Horizontal lines
-	for y in range(1, total_height):
+	for y in range(0, total_height + 1):
 		var color = Color.DARK_GRAY
 		var width = -1
 		if y == base_height or y == total_height - base_height:
 			color = Color.LIGHT_GRAY
 			width = 3
-		draw_line(Vector2i(horizontal_start, tile_height * y), Vector2i(horizontal_end, tile_height * y), color, width)
+		draw_line(Vector2i(horizontal_start, tile_height * y + vertical_start), Vector2i(horizontal_end, tile_height * y + vertical_start), color, width)
 	
 	# Vertical lines
-	for x in range(1, board_width):
+	for x in range(0, board_width + 1):
 		var color = Color.DARK_GRAY
 		var width = -1
-		draw_line(Vector2i(tile_width * x, vertical_start), Vector2i(tile_width * x, vertical_end), color, width)
+		draw_line(Vector2i(tile_width * x + horizontal_start, vertical_start), Vector2i(tile_width * x + horizontal_start, vertical_end), color, width)
 
 func tile_coords_to_position(x: int, y: int) -> Vector2i:
 	return Vector2i(x * tile_width, y * tile_height)
 
 func tile_coords_to_index(x: int, y: int) -> int:
 	return x * board_width + y
+
+enum Building {
+	Trench,
+	Bunker,
+}
+
+enum Unit {
+	Squad,
+}
